072eae55d2610a8cfd4dfa3d42b37a7632dc0d01
Florian Klein <florian.klein02@gmx.net>
2025-10-03 17:56:06 +0200

    implemented a init version of FrameAggregation and Late Fusion based on Simple2DCNN and Simple3DCNN classes defined earlier

--- models.py
+++ models.py
@@ -2,8 +2,6 @@ import torch
 import torch.nn as nn
 
 
-
-
 class Simple2DCNN(nn.Module):
 
     def __init__(self, num_classes=None, in_channels=3):
@@ -66,7 +64,70 @@ class Simple3DCNN(nn.Module):
         if self.fc1:
             return self.fc1(x)
         return x
+  
+    
+
+class FrameAggregation2D(nn.Module):
+
+    def __init__(self, num_classes):
+        super().__init__()
+        self.backbone = Simple2DCNN(num_classes=num_classes)
+        self.fc = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        B, T, C, H, W = x.shape
+        x = x.view(B * T, C, H, W)
+        feats = self.backbone(x)
+        feats = feats.view(B, T, -1).mean(dim=1)
+        return self.fc(feats)
+
+
+class FrameAggregation3D(nn.Module):
+
+    def __init__(self, num_classes):
+        super().__init__()
+        self.backbone = Simple3DCNN(num_classes=num_classes)
+        self.fc = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        B, N, C, T, H, W = x.shape
+        x = x.view(B*N, C, T, H, W)
+        feats = self.backbone(x)
+        feats = x.view(B, N, -1).mean(dim=1)
+        return self.fc(feats)
+
+
+
+class LateFusion2D(nn.Module):
+
+    def __init__(self, num_classes):
+        super().__init__()
+        self.rgb_model = Simple2DCNN(num_classes=num_classes)
+        self.flow_model = Simple2DCNN(num_classes=num_classes)
+
+        self.fc = nn.Linear(2 * 256, num_classes)
+
+    def forward(self, rgb, flow):
+        B, T, C, H, W = rgb.shape
+        rgb_feats = self.rgb_model(rgb.view(B*T, C, H , W).view(B, T, -1)).mean(1)
+        flow_feats = self.flow_model(flow.view(B*T, C, H , W).view(B, T, -1)).mean(1)
+        fused = torch.cat([rgb_feats, flow_feats], dim=1)
+        return self.fc(fused)
     
 
 
+class LateFusion3D(nn.Module):
+
+    def __init__(self, num_classes):
+        super().__init__()
+        self.rgb_model = Simple3DCNN(num_classes=num_classes)
+        self.flow_model = Simple3DCNN(num_classes=num_classes)
+
+        self.fc = nn.Linear(2 * 256, num_classes)
+
+    def forward(self, rgb, flow):
+        rgb_feats = self.rgb_model(rgb)
+        flow_feats = self.flow_model(flow)
+        fused = torch.cat([rgb_feats, flow_feats], dim=1)
+        return self.fc(fused)
 
